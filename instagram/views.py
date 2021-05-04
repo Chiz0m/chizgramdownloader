e10ba07775c59881260f5c3538b5b9191ee7a686
Chizom Echehieuka <chizom@Chizoms-MacBook-Air.local>
2021-05-04 10:41:14 +0100

    added instagram api call

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import InstagramSerializer
+from .models import Instagram
+import requests
 
 # Create your views here.
+@api_view(['POST'])
+def saveDownloadIg(request):
+    serializer = InstagramSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        url = serializer.data['url']
+        querystring = {"igurl":url}
+        headers = {
+        'x-rapidapi-key': "6400ff6b24msh0777960a581cf0ap19f8c6jsn307329deaa78",
+        'x-rapidapi-host': "instagram-facebook-media-downloader.p.rapidapi.com"
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        return Response(response)
+    else:
+        return Response(serializer.errors)
